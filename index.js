bec5edfaf0ebf086756b667ee832de91fcbbdeb2
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2021-07-24 14:58:10 +0300

    constructor function

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ const creatCircle1 = createCircle(1);
 console.log(creatCircle1);
 const createCircle2 = createCircle(2);
 console.log(createCircle2);
+
+//Constructor function.
+function Circle(radius) {
+    this.radius = 'radius',
+    this.draw = function(){
+        console.log('draw')
+    }
+}
+const circle = new Circle(1);
+console.log(circle);
